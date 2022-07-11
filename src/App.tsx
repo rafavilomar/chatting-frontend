c0cb0fbc06767cdd9ffbc4f8a0746080b928e24d
Rafael Vilomar <rafael.vilomar@mqaamericas.com>
2022-07-10 20:38:44 -0400

    chore: add socket connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
+import socket from "./utils/socket.js";
 
 function App() {
+  useEffect(() => {
+    socket.emit("connection", "Hi from client");
+  }, []);
   return (
     <div>
       <h1>Chatting</h1>
