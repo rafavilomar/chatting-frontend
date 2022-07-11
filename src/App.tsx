74950ef7c4304d5a2c0dedf8199ea5c3941f7a81
Rafael Vilomar <rafael.vilomar@mqaamericas.com>
2022-07-10 21:16:56 -0400

    chore: implement chakra-ui #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import React, { useEffect } from "react";
+import { Heading } from "@chakra-ui/react";
+
 import socket from "./utils/socket.js";
 
 function App() {
   useEffect(() => {
     socket.emit("connection", "Hi from client");
   }, []);
-  return (
-    <div>
-      <h1>Chatting</h1>
-    </div>
-  );
+  return <Heading>Chatting</Heading>;
 }
 
 export default App;
