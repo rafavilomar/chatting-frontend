a3e956db152e843006bff61cd33294f6b809ec35
Rafael Vilomar <rafael.vilomar@mqaamericas.com>
2022-07-14 22:24:55 -0400

    feat: add user list with avatar component from chakra ui #9

--- src/component/ActiveUserList.tsx
+++ src/component/ActiveUserList.tsx
@@ -6,8 +6,17 @@ import {
   ModalHeader,
   ModalOverlay,
 } from "@chakra-ui/modal";
+import { Divider, Flex } from "@chakra-ui/react";
 import React, { useContext } from "react";
 import GeneralContext from "../utils/context/context";
+import UserElement from "./UserElement";
+
+const userList = [
+  { username: "user1" },
+  { username: "user2" },
+  { username: "user3" },
+  { username: "user4" },
+];
 
 const ActiveUserList = () => {
   const { openActiveUserList, handleActiveUserList } = useContext(
@@ -22,13 +31,23 @@ const ActiveUserList = () => {
         >
           <ModalOverlay />
           <ModalContent>
-            <ModalHeader>Modal Title</ModalHeader>
+            <ModalHeader>Active Users</ModalHeader>
             <ModalCloseButton />
-            <ModalBody>
-              "Lorem, ipsum dolor sit amet consectetur adipisicing elit. In
-              temporibus eos velit natus accusamus ducimus, doloremque at sed
-              ipsum voluptatum, obcaecati cum sapiente unde aspernatur iure!
-              Enim nisi commodi consequatur!",
+            <ModalBody
+              paddingLeft={0}
+              paddingTop={0}
+              paddingRight={0}
+              paddingBottom="2"
+            >
+              <Flex gap={2} direction="column">
+                {userList.map((user, index) => (
+                  <>
+                    {index === 0 && <Divider />}
+                    <UserElement username={user.username} />
+                    {index < userList.length - 1 && <Divider />}
+                  </>
+                ))}
+              </Flex>
             </ModalBody>
           </ModalContent>
         </Modal>
